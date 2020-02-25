f62387847643050bed4b705d64cef0f28e698d91
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-02-25 01:12:55 -0300

    Adding properties, getters and setters to Node

--- src/newtonscavazzini/stack/Node.java
+++ src/newtonscavazzini/stack/Node.java
@@ -1,4 +1,24 @@
 package newtonscavazzini.stack;
 
 public class Node {
+
+    private int value;
+    private Node next;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 }

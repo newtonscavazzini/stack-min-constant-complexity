da59c212186ea93f99e89731f76c17d0778acdd7
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-02-26 01:51:27 -0300

    Creating class for the special Stack called StackCC

---
+++ src/newtonscavazzini/stack/StackCC.java
@@ -0,0 +1,15 @@
+package newtonscavazzini.stack;
+
+/**
+ * This is the implementation of the special Stack (CC stands for Constant Complexity).
+ * All of the methods of this Stack returns in O(1) time.
+ */
+public class StackCC extends Stack {
+
+    protected Stack minStack;
+
+    public StackCC() {
+        this.minStack = new Stack();
+    }
+
+}

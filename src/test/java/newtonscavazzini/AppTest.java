584a4b48202abaee517add8b5809744eecebab6e
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-02-26 17:28:40 -0300

    Creating first test class

---
+++ src/test/java/newtonscavazzini/AppTest.java
@@ -0,0 +1,16 @@
+package newtonscavazzini;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void testing() {
+        assertTrue(true);
+        assertEquals(1, 1);
+    }
+
+}

0c1c58893a3a178f3d6c8de350b149d751d70d99
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-02-26 00:11:14 -0300

    Implementing push(int) on basic Stack

--- src/newtonscavazzini/stack/Stack.java
+++ src/newtonscavazzini/stack/Stack.java
@@ -9,4 +9,20 @@ public class Stack {
     protected Node top;
     protected int size;
 
+    /**
+     * Pushes an element to the top of the Stack.
+     * Time Complexity: O(1)
+     * @param value Value to be pushed.
+     */
+    public void push(int value) {
+        Node newNode = new Node(value);
+
+        if (this.top != null) {
+            newNode.setNext(this.top);
+        }
+
+        this.top = newNode;
+        this.size++;
+    }
+
 }
